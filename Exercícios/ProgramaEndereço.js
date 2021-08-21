3802f4e06b58803689c563a30eb9fd749c0bd8e2
Pedro Seibel <64672223+pedroseibel@users.noreply.github.com>
2021-08-21 19:39:24 -0300

    Create ProgramaEndereço.js

---
+++ Exercícios/ProgramaEndereço.js
@@ -0,0 +1,6 @@
+//Elabore um programa que escreve seu nome completo na primeira linha, seu endereço na segunda, o CEP na terceira e o telefone na quarta.
+
+console.log("Pedro Seibel")
+console.log("Rua do Medo")
+console.log("CEP: 12345678")
+console.log("Telefone: 55999999999")
