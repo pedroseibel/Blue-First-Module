72256938e998e21caa5284d670d6d8b1d34a2ecf
Pedro Seibel <64672223+pedroseibel@users.noreply.github.com>
2021-08-21 19:46:32 -0300

    Create Letra.js

---
+++ Exercícios/Letra.js
@@ -0,0 +1,15 @@
+//Faça um programa que mostre na tela uma letra de música que você gosta.
+
+console.log("I couldn’t wait for no man\n"+
+"Divided your face in freehand, because\n"+
+
+"There’s context in our love\n"+
+
+"Riding away on horseback\nCarving a name, the romance had gone\n"+
+
+"Rolled on your instep\n"+
+"Firing in ways that I won’t\n"+
+"Hiding your waist and\n"+
+"Probably placed that I don’t\n"+
+
+"Oh, there’s contact")
