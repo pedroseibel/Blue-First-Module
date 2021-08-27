a13007d89be0b91472d8763df7edc9ab7549ab0e
Pedro Seibel <64672223+pedroseibel@users.noreply.github.com>
2021-08-27 15:51:17 -0300

    Create CalculadoraIdade.js

---
+++ Exercícios/CalculadoraIdade.js
@@ -0,0 +1,10 @@
+// Defina uma variável para o valor do ano do nascimento;
+// Defina uma variável para o valor do ano atual;
+// Defina uma variável que calcula o valor final da idade da pessoa;
+// Exiba uma mensagem final dizendo a idade da pessoa e a mensagem "Você está na flor da idade".
+
+let anoNascimento = prompt("Digite o ano do seu nascimento: ");
+let AD = 2021;
+let idade = AD - anoNascimento;
+
+console.log("Você tem " + idade + " anos" + ". \nVocê está na flor da idade.");
