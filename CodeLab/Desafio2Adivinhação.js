4ce3bad30ca0b4005bbfdd8ddf8551e9f71d7045
Pedro Seibel <64672223+pedroseibel@users.noreply.github.com>
2021-08-27 16:18:25 -0300

    Update Desafio2Adivinhação.js

--- CodeLab/Desafio2Adivinhação.js
+++ CodeLab/Desafio2Adivinhação.js
@@ -2,12 +2,12 @@
 Jogo da adivinhação
 Escreva um programa que faça o computador “pensar” em um número inteiro entre 0 e 10 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu.*/
 
-var num = parseInt(prompt())
+let numero = parseInt(prompt("Tente adivinhar o número que o computador escolherá: "));
 
-var otherNum = Math.floor(Math.random() * 10 + 1)
+let numeroPc = Math.floor(Math.random() * 10 + 1);
 
-if(num == otherNum){
-  console.log("Você acertou!")
-} else{
-  console.log("Você errou! O número era " + otherNum)
+if (numero == numeroPc) {
+  console.log("Você adivinhou o número!");
+} else {
+  console.log("Você errou! O número era: " + numeroPc);
 }
